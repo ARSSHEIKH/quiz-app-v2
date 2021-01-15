ea33d5182374513d97f002972c0d948cfd6f6c9e
Arsalan Sheikh <41226869+ARSSHEIKH@users.noreply.github.com>
2021-01-15 05:20:34 +0500

    created build

---
+++ build/precache-manifest.fa0efd8d4cb40a08bef042261197a409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1250f7d79c0a9a00ba5009023422a328",
+    "url": "/index.html"
+  },
+  {
+    "revision": "423babc0d250b01a4ffc",
+    "url": "/static/css/main.e5db26bb.chunk.css"
+  },
+  {
+    "revision": "f990788a51cb5a53d0d5",
+    "url": "/static/js/2.d3b7bbf5.chunk.js"
+  },
+  {
+    "revision": "734f826f1cbbfd351e70a1d4994558ad",
+    "url": "/static/js/2.d3b7bbf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "423babc0d250b01a4ffc",
+    "url": "/static/js/main.16737e9a.chunk.js"
+  },
+  {
+    "revision": "0ede2f893ca96fe348c1",
+    "url": "/static/js/runtime-main.863cf357.js"
+  }
+]);
